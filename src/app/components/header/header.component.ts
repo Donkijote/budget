940ee0444297c825626a9c7accf970122e546097
Donkijote <manuelmjgc@gmail.com>
2020-07-25 18:56:09 -0400

    setting up side menu, dynamic title in landing page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() Title: string;
+  @Input() Menu: boolean;
   constructor() {}
 
   ngOnInit() {}
