7f3be5bc3c1a32478a8ba05b036abd59b50aac7c
Donkijote <manuelmjgc@gmail.com>
2020-07-26 17:07:44 -0400

    showing totals

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,7 @@ import {
 import { AddBudgetComponent } from './add-budget/add-budget.component';
 import { IBudget, Budget } from '../models/budget';
 import { BudgetService } from '../services/budget.service';
-import { Subscription } from 'rxjs';
+import { Subscription, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -21,8 +21,11 @@ export class HomePage implements OnInit, OnDestroy {
   @ViewChild('dateTime', { static: true }) datePicker: IonDatetime;
   loadedBudgets: Budget[];
   title: string;
-  date = new Date().toISOString();
+  date = new BehaviorSubject<string>(new Date().toISOString());
   isLoading = false;
+  totalIn: number;
+  totalOut: number;
+  totalSum: number;
   constructor(
     private modal: ModalController,
     private loading: LoadingController,
@@ -31,10 +34,26 @@ export class HomePage implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.title = this.date;
+    this.date.subscribe((date) => {
+      this.title = date;
+    });
 
     this.budgetSub = this.budgetService.budgets.subscribe((budgets) => {
       this.loadedBudgets = budgets;
+      let inc = 0;
+      let out = 0;
+
+      for (const i of budgets) {
+        if (i.type === 1) {
+          inc += i.amount;
+        } else if (i.type === 2) {
+          out += i.amount;
+        }
+      }
+
+      this.totalIn = inc;
+      this.totalOut = out;
+      this.totalSum = inc - out;
     });
   }
 
@@ -46,14 +65,16 @@ export class HomePage implements OnInit, OnDestroy {
 
   ionViewWillEnter(): void {
     this.isLoading = true;
-    this.budgetService.fetchBudgets(this.date.split('T')[0]).subscribe(() => {
-      this.isLoading = false;
+    this.date.subscribe((date) => {
+      this.budgetService.fetchBudgets(date.split('T')[0]).subscribe(() => {
+        this.isLoading = false;
+      });
     });
   }
 
   updateMyDate($event: any) {
     const pickedDate = new Date($event.detail.value);
-    this.title = pickedDate.toISOString();
+    this.date.next(pickedDate.toISOString());
   }
 
   onOpenCalendar(): void {
@@ -71,37 +92,39 @@ export class HomePage implements OnInit, OnDestroy {
         return el.onDidDismiss();
       })
       .then((result) => {
-        const budget: IBudget = result.data;
-        budget.date = this.date.split('T')[0];
-        this.loading
-          .create({
-            message: 'Guardando transaccion',
-          })
-          .then((el) => {
-            el.present();
-            this.budgetService.addNewBudget(budget).subscribe(
-              (resp) => {
-                el.dismiss();
-                console.log(resp);
-              },
-              (err) => {
-                el.dismiss();
-                const message = err.error.error;
-                this.alert
-                  .create({
-                    buttons: [
-                      {
-                        text: 'Ok',
-                        cssClass: 'text-danger',
-                      },
-                    ],
-                    message,
-                    backdropDismiss: false,
-                  })
-                  .then((a) => a.present());
-              }
-            );
-          });
+        if (result.data) {
+          const budget: IBudget = result.data;
+          budget.date = this.title.split('T')[0];
+          this.loading
+            .create({
+              message: 'Guardando transaccion...',
+            })
+            .then((el) => {
+              el.present();
+              this.budgetService.addNewBudget(budget).subscribe(
+                (resp) => {
+                  el.dismiss();
+                  console.log(resp);
+                },
+                (err) => {
+                  el.dismiss();
+                  const message = err.error.error;
+                  this.alert
+                    .create({
+                      buttons: [
+                        {
+                          text: 'Ok',
+                          cssClass: 'text-danger',
+                        },
+                      ],
+                      message,
+                      backdropDismiss: false,
+                    })
+                    .then((a) => a.present());
+                }
+              );
+            });
+        }
       });
   }
 
